b198b1d323ea66c263fcf09bc29f54bbfc80d166
Oluwafemi Oyedele <oluwafemioyedele908@gmail.com>
2021-11-15 07:18:14 -1100

    I added installation files

---
+++ scripts/Installation.R
@@ -0,0 +1,11 @@
+install.packages(c('knitr','skimr','tidyverse','readxl','esquisse','ggThemeAssist','here','devtools'))
+
+
+
+
+## Training packages 2  ####
+
+install.packages (c("agridat","dendextend", "emmeans",
+"lmerTest", "pbkrtest", "RColorBrewer", "devtools", "statgenSTA", "statgenGxE")) 
+
+#https://cran.r-project.org/bin/windows/Rtools/rtools40v2-x86_64.exe
